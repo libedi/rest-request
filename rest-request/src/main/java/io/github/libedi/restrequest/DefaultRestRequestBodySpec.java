f303a5ac8c94f738fa33604a48bbb5fd28104a52
Sangjun, Park <libedi@gmail.com>
2022-05-29 22:23:01 +0900

    #22-Add file upload feature - supported type : File, Path, MultipartFile - add jackson-databind dependency - add test file

--- rest-request/src/main/java/io/github/libedi/restrequest/DefaultRestRequestBodySpec.java
+++ rest-request/src/main/java/io/github/libedi/restrequest/DefaultRestRequestBodySpec.java
@@ -1,13 +1,30 @@
 package io.github.libedi.restrequest;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.SpringVersion;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.libedi.restrequest.RestRequestSpec.RestRequestBodySpec;
 
@@ -21,42 +38,123 @@ import io.github.libedi.restrequest.RestRequestSpec.RestRequestBodySpec;
 public class DefaultRestRequestBodySpec<T> extends DefaultRestRequestFormSpec<T, RestRequestBodySpec<T>>
 		implements RestRequestBodySpec<T> {
 
+    private final Pattern VERSION_PREFIX_PATTERN = Pattern.compile("(^[0-9]\\.[0-9]).*");
+
 	private Object body;
+    private boolean isMultipart;
 
 	DefaultRestRequestBodySpec(final URI uri, final HttpMethod method, final Class<T> responseType,
 			final ParameterizedTypeReference<T> typeReference) {
 		super(uri, method, responseType, typeReference);
 	}
 
-	@Override
+    @SuppressWarnings("unchecked")
+    @Override
 	public RestRequestBodySpec<T> body(final Object body) {
-		this.body = body;
+	    if (body instanceof MultiValueMap) {
+	        setParams((MultiValueMap<String, Object>) body);
+	    } else {
+	        this.body = body;
+	    }
 		return this;
 	}
 
+    @Override
+    public RestRequestBodySpec<T> addFile(final String key, final File file) {
+        addParam(key, new FileSystemResource(file));
+        isMultipart = true;
+        return this;
+    }
+
+    @Override
+    public RestRequestBodySpec<T> addFile(final String key, final Path path) {
+        addParam(key, new FileSystemResource(path.toFile()));
+        isMultipart = true;
+        return this;
+    }
+
+    @Override
+    public RestRequestBodySpec<T> addFile(final String key, final MultipartFile multipartFile) {
+        try {
+            addParam(key, new ByteArrayResource(multipartFile.getBytes()) {
+                @Override
+                public String getFilename() {
+                    return multipartFile.getOriginalFilename();
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        isMultipart = true;
+        return this;
+    }
+
 	@Override
 	public RestRequest<T> build() {
+        setMultipartData();
 		return new RestRequest<>(getUriWithQueryParam(), getMethod(), makeHttpEntity(), getResponseType(),
 				getTypeReference());
 	}
 
+    private void setMultipartData() {
+        if (!isMultipart && getParameter().entrySet().stream().anyMatch(this::hasMultipartFormData)) {
+            isMultipart = true;
+        }
+        if (!isMultipart) {
+            return;
+        }
+        if (body == null) {
+            changeMultipartContentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+        } else if (isSupportedMultipartMixed()) {
+            changeMultipartContentType("multipart/mixed");
+            try {
+                addParam("body", new ObjectMapper().writeValueAsString(body));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private boolean hasMultipartFormData(final Entry<String, List<Object>> entry) {
+        return !CollectionUtils.isEmpty(entry.getValue()) && entry.getValue().get(0) instanceof Resource;
+    }
+
+    private void changeMultipartContentType(final String multipartContentType) {
+        final MediaType contentType = getHeaders().getContentType();
+        if (contentType == null || !StringUtils.startsWithIgnoreCase(contentType.getType(), "multipart")) {
+            contentType(multipartContentType);
+        }
+    }
+
 	private URI getUriWithQueryParam() {
 		final UriComponentsBuilder builder = UriComponentsBuilder.fromUri(getUri());
 		if (hasQueryParameter()) {
-			for (final Entry<String, List<Object>> entry : getParameter().entrySet()) {
-				builder.queryParam(entry.getKey(),
-						entry.getValue() != null ? entry.getValue().toArray() : new Object[0]);
-			}
+            getParameter().entrySet().stream()
+                    .filter(entry -> !hasMultipartFormData(entry))
+                    .forEach(entry -> builder.queryParam(entry.getKey(),
+                            CollectionUtils.isEmpty(entry.getValue()) ? new Object[0] : entry.getValue().toArray()));
 		}
 		return builder.build().toUri();
 	}
 
 	private boolean hasQueryParameter() {
-		return getParameter() != null && body != null;
+        return getParameter() != null && body != null && !isAvaliableMultipartMixedData();
 	}
 
+    private boolean isAvaliableMultipartMixedData() {
+        return isMultipart && isSupportedMultipartMixed();
+    }
+
+    private boolean isSupportedMultipartMixed() {
+        final Matcher matcher = VERSION_PREFIX_PATTERN.matcher(SpringVersion.getVersion());
+        if (matcher.find()) {
+            return Double.valueOf(matcher.group(1)).doubleValue() >= 5.2;
+        }
+        return false;
+    }
+
 	private HttpEntity<?> makeHttpEntity() {
-		return new HttpEntity<>(body != null ? body : getParameter(), getHeaders());
+        return new HttpEntity<>(body == null || isAvaliableMultipartMixedData() ? getParameter() : body, getHeaders());
 	}
 
 }
